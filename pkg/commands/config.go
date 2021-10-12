1dcab158d73b259c6e1c3df56f18626a46c266d6
Jason Hall <jasonhall@redhat.com>
2021-10-12 15:04:15 -0400

    WIP: allow specifying multiple base images

--- pkg/commands/config.go
+++ pkg/commands/config.go
@@ -18,6 +18,8 @@ package commands
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -32,6 +34,8 @@ import (
 	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/daemon"
+	"github.com/google/go-containerregistry/pkg/v1/empty"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/google/go-containerregistry/pkg/v1/types"
 	"github.com/google/ko/pkg/build"
@@ -63,80 +67,145 @@ func getBaseImage(platform string, bo *options.BuildOptions) build.GetBase {
 		//    github.com/GoogleCloudPlatform/foo/cmd/bar
 		// comes through as:
 		//    github.com/googlecloudplatform/foo/cmd/bar
-		baseImage, ok := baseImageOverrides[strings.ToLower(s)]
+		baseImages, ok := baseImageOverrides[strings.ToLower(s)]
 		if !ok {
-			baseImage = defaultBaseImage
+			baseImages = defaultBaseImage
 		}
 		if bo.BaseImage != "" {
-			baseImage = bo.BaseImage
+			baseImages = bo.BaseImage
 		}
 		nameOpts := []name.Option{}
 		if bo.InsecureRegistry {
 			nameOpts = append(nameOpts, name.Insecure)
 		}
-		ref, err := name.ParseReference(baseImage, nameOpts...)
+		var idxs []v1.ImageIndex
+		var ref name.Reference
+		for _, baseRef := range strings.Split(baseImages, ",") {
+			var err error
+			ref, err = name.ParseReference(baseRef, nameOpts...)
+			if err != nil {
+				return nil, nil, fmt.Errorf("parsing base image (%q): %v", baseRef, err)
+			}
+			// For ko.local, look in the daemon.
+			if ref.Context().RegistryStr() == publish.LocalDomain {
+				if strings.Contains(baseImages, ",") {
+					return nil, nil, errors.New("cannot specify multiple base images when one uses the local domain")
+				}
+				img, err := daemon.Image(ref)
+				return ref, img, err
+			}
+
+			userAgent := ua()
+			if bo.UserAgent != "" {
+				userAgent = bo.UserAgent
+			}
+			ropt := []remote.Option{
+				remote.WithAuthFromKeychain(authn.DefaultKeychain),
+				remote.WithUserAgent(userAgent),
+				remote.WithContext(ctx),
+			}
+
+			// Using --platform=all will use an image index for the base,
+			// otherwise we'll resolve it to the appropriate platform.
+			//
+			// Platforms can be comma-separated if we only want a subset of the base
+			// image.
+			multiplatform := platform == "all" || strings.Contains(platform, ",")
+			var p v1.Platform
+			if platform != "" && !multiplatform {
+				parts := strings.Split(platform, "/")
+				if len(parts) > 0 {
+					p.OS = parts[0]
+				}
+				if len(parts) > 1 {
+					p.Architecture = parts[1]
+				}
+				if len(parts) > 2 {
+					p.Variant = parts[2]
+				}
+				if len(parts) > 3 {
+					return nil, nil, fmt.Errorf("too many slashes in platform spec: %s", platform)
+				}
+				ropt = append(ropt, remote.WithPlatform(p))
+			}
+
+			log.Printf("Using base %s for %s", ref, s)
+			desc, err := remote.Get(ref, ropt...)
+			if err != nil {
+				return nil, nil, err
+			}
+			switch desc.MediaType {
+			case types.OCIImageIndex, types.DockerManifestList:
+				if multiplatform {
+					idx, err := desc.ImageIndex()
+					if err != nil {
+						return nil, nil, err
+					}
+					idxs = append(idxs, idx)
+					continue
+				}
+				img, err := desc.Image()
+				return ref, img, err
+			default:
+				if strings.Contains(baseImages, ",") {
+					return nil, nil, errors.New("cannot specify multiple base images when one is a single-platform image")
+				}
+				img, err := desc.Image()
+				return ref, img, err
+			}
+		}
+		idx, err := combine(idxs)
+		if len(idxs) != 1 {
+			ref = nil
+		}
+		return ref, idx, err
+	}
+}
+
+func combine(idxs []v1.ImageIndex) (v1.ImageIndex, error) {
+	if len(idxs) == 1 {
+		return idxs[0], nil
+	}
+
+	plats := map[string]bool{}
+	var adds []mutate.IndexAddendum
+	add := func(idx v1.ImageIndex) error {
+		mf, err := idx.IndexManifest()
 		if err != nil {
-			return nil, nil, fmt.Errorf("parsing base image (%q): %v", baseImage, err)
+			return fmt.Errorf("IndexManifest: %w", err)
 		}
+		for _, desc := range mf.Manifests {
+			if desc.Platform == nil {
+				return fmt.Errorf("found nil platform for manifest %s", desc.Digest)
+			}
 
-		// For ko.local, look in the daemon.
-		if ref.Context().RegistryStr() == publish.LocalDomain {
-			img, err := daemon.Image(ref)
-			return ref, img, err
-		}
+			b, err := json.Marshal(desc.Platform)
+			if err != nil {
+				return fmt.Errorf("marshalling platform: %w", err)
+			}
+			if plats[string(b)] {
+				return fmt.Errorf("conflicting platform %+v", *desc.Platform)
+			}
+			plats[string(b)] = true
 
-		userAgent := ua()
-		if bo.UserAgent != "" {
-			userAgent = bo.UserAgent
+			img, err := idx.Image(desc.Digest)
+			if err != nil {
+				return fmt.Errorf("getting image %s: %w", desc.Digest, err)
+			}
+			adds = append(adds, mutate.IndexAddendum{
+				Add:        img,
+				Descriptor: desc,
+			})
 		}
-		ropt := []remote.Option{
-			remote.WithAuthFromKeychain(authn.DefaultKeychain),
-			remote.WithUserAgent(userAgent),
-			remote.WithContext(ctx),
-		}
-
-		// Using --platform=all will use an image index for the base,
-		// otherwise we'll resolve it to the appropriate platform.
-		//
-		// Platforms can be comma-separated if we only want a subset of the base
-		// image.
-		multiplatform := platform == "all" || strings.Contains(platform, ",")
-		var p v1.Platform
-		if platform != "" && !multiplatform {
-			parts := strings.Split(platform, "/")
-			if len(parts) > 0 {
-				p.OS = parts[0]
-			}
-			if len(parts) > 1 {
-				p.Architecture = parts[1]
-			}
-			if len(parts) > 2 {
-				p.Variant = parts[2]
-			}
-			if len(parts) > 3 {
-				return nil, nil, fmt.Errorf("too many slashes in platform spec: %s", platform)
-			}
-			ropt = append(ropt, remote.WithPlatform(p))
-		}
-
-		log.Printf("Using base %s for %s", ref, s)
-		desc, err := remote.Get(ref, ropt...)
-		if err != nil {
-			return nil, nil, err
-		}
-		switch desc.MediaType {
-		case types.OCIImageIndex, types.DockerManifestList:
-			if multiplatform {
-				idx, err := desc.ImageIndex()
-				return ref, idx, err
-			}
-			img, err := desc.Image()
-			return ref, img, err
-		default:
-			img, err := desc.Image()
-			return ref, img, err
+		return nil
+	}
+	for _, idx := range idxs {
+		if err := add(idx); err != nil {
+			return nil, err
 		}
 	}
+
+	return mutate.AppendManifests(mutate.IndexMediaType(empty.Index, types.DockerManifestList), adds...), nil
 }
 
 func getTimeFromEnv(env string) (*v1.Time, error) {
@@ -240,19 +309,23 @@ func loadConfig(workingDirectory string) error {
 		}
 	}
 
-	ref := v.GetString("defaultBaseImage")
-	if _, err := name.ParseReference(ref); err != nil {
-		return fmt.Errorf("'defaultBaseImage': error parsing %q as image reference: %v", ref, err)
+	refs := v.GetString("defaultBaseImage")
+	for _, ref := range strings.Split(refs, ",") {
+		if _, err := name.ParseReference(ref); err != nil {
+			return fmt.Errorf("'defaultBaseImage': error parsing %q as image reference: %v", ref, err)
+		}
 	}
-	defaultBaseImage = ref
+	defaultBaseImage = refs
 
 	baseImageOverrides = make(map[string]string)
 	overrides := v.GetStringMapString("baseImageOverrides")
-	for key, value := range overrides {
-		if _, err := name.ParseReference(value); err != nil {
-			return fmt.Errorf("'baseImageOverrides': error parsing %q as image reference: %v", value, err)
+	for key, values := range overrides {
+		for _, value := range strings.Split(values, ",") {
+			if _, err := name.ParseReference(value); err != nil {
+				return fmt.Errorf("'baseImageOverrides': error parsing %q as image reference: %v", value, err)
+			}
 		}
-		baseImageOverrides[key] = value
+		baseImageOverrides[key] = values
 	}
 
 	var builds []build.Config
